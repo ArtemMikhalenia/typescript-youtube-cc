7b005899bb3729316cdccfd57a2b0fdd8152fe3d
Artem <goldtree3000@gmail.com>
2024-02-18 18:47:34 +0300

    feat: add other modules

--- 2_interfaces.ts
+++ 2_interfaces.ts
@@ -1,77 +1,75 @@
 interface Rect {
-  readonly id: string
-  color?: string
-  size: {
-    width: number
-    height: number
-  }
+	readonly id: string;
+	color?: string;
+	size: {
+		width: number;
+		height: number;
+	};
 }
 
 const rect1: Rect = {
-  id: '1234',
-  size: {
-    width: 20,
-    height: 30
-  },
-  color: '#ccc'
-}
+	id: "1234",
+	size: {
+		width: 20,
+		height: 30,
+	},
+	color: "#ccc",
+};
 
 const rect2: Rect = {
-  id: '12345',
-  size: {
-    width: 10,
-    height: 5
-  }
-}
+	id: "12345",
+	size: {
+		width: 10,
+		height: 5,
+	},
+};
 
-rect2.color = 'black'
-// rect2.id = '3232'
+rect2.color = "#fff";
+// rect2.id = "2222";
 
-const rect3 = {} as Rect
-const rect4 = <Rect>{}
+const rect3 = {} as Rect;
+const rect4 = <Rect>{};
 
-// =====================
+//=====================================
 
 interface RectWithArea extends Rect {
-  getArea: () => number
+	getArea: () => number;
 }
 
 const rect5: RectWithArea = {
-  id: '123',
-  size: {
-    width: 20,
-    height: 20
-  },
-  getArea(): number {
-    return this.size.width * this.size.height
-  }
-}
+	id: "123",
+	size: {
+		width: 20,
+		height: 20,
+	},
 
-// ==================
+	getArea(): number {
+		return this.size.width * this.size.height;
+	},
+};
+
+//=================================
 
 interface IClock {
-  time: Date
-  setTime(date: Date): void
+	time: Date;
+	setTime(date: Date): void;
 }
 
 class Clock implements IClock {
-  time: Date = new Date()
-
-  setTime(date: Date): void {
-    this.time = date
-  }
+	time: Date = new Date();
+	setTime(date: Date): void {
+		this.time = date;
+	}
 }
 
-// =================
+//================================
 
 interface Styles {
-  [key: string]: string
+	[key: string]: string
 }
 
 const css: Styles = {
-  border: '1px solid black',
-  marginTop: '2px',
-  borderRadius: '5px'
-}
-
-
+	border: "1px solid black",
+	marginTop: "2px",
+	borderRadius: "5px",
+};
--- 3_enum.ts
+++ 3_enum.ts
@@ -1,20 +1,20 @@
 enum Membership {
-  Simple,
-  Standard,
-  Premium
+	Simple,
+	Standard,
+	Premium,
 }
 
-const membership = Membership.Standard
-const membershipReverse = Membership[2]
+const membership = Membership.Standard;
+const membershipReverse = Membership[2];
 
-console.log(membership)
-console.log(membershipReverse)
+console.log(membership);
+console.log(membershipReverse);
 
 enum SocialMedia {
-  VK = 'VK',
-  FACEBOOK = 'FACEBOOK',
-  INSTAGRAM = 'INSTAGRAM'
+	VK = "VK",
+	FACEBOOK = "FACEBOOK",
+	INSTAGRAM = "INSTAGRAM",
 }
 
-const social = SocialMedia.INSTAGRAM
-console.log(social)
+const social = SocialMedia.INSTAGRAM;
+console.log(social);
